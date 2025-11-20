f9dd7dcab6555c80ed05c7c3a60969433ac24677
RamsesMaza <rmazaapaza@gmail.com>
2025-11-19 22:05:26 -0500

    nuevo

--- api/api-entry/index.js
+++ api/api-entry/index.js
@@ -1,4 +1,5 @@
 const createHandler = require("azure-function-express").createHandler;
-const app = require("../dist/src/app.js");
+
+const app = require("../dist/app.js"); 
 
 module.exports = createHandler(app.default || app);
